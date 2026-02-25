344333bec7d5c6fe56430ac27e203e08556da0dd
Tristan <tristan.dev03@gmail.com>
2026-02-24 19:26:24 -0500

    initial symbol set initialization

---
+++ JackpotTherapy/JackpotTherapy/slots/slotmachine.py
@@ -0,0 +1,25 @@
+from random import random
+from time import time
+
+line_max = 3
+bet_max = 100
+bet_min = 1
+
+rows = 3
+columns = 3
+
+symbol_set
+symbol_count = {
+    "diamond": 1
+    "crown": 2
+    "horseshoe": 3
+    "clover": 4
+    "trophy": 5
+    "$": 6
+    "star": 7
+    "bell": 8
+    "coin": 9
+}
+
+symbols = [diamond, crown, horseshoe, clover, trophy, $, star, bell, coin]
+def slot_machine_spin(rows, columns)
