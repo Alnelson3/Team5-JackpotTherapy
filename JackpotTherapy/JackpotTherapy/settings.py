21ada502e2ca944f58a9b4da027f174a80007167
Nuno <61714135+NunoR1@users.noreply.github.com>
2026-03-30 00:29:04 -0400

    Updated a couple of settings

--- JackpotTherapy/JackpotTherapy/settings.py
+++ JackpotTherapy/JackpotTherapy/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-    BASE_DIR / "Jackpot_Therapy" / "static",
+    # BASE_DIR / "staticfiles",
+    BASE_DIR / "Jackpot_Therapy" / "static"
 ]
 
 STATIC_ROOT = BASE_DIR / "static_root"
